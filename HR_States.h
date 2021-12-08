f26aec21b3aaa9befcf74eee4415770e7afc21e4
Seth <sethabrittain@gmail.com>
2021-12-08 12:19:44 -0600

    Current "Working"

--- HR_States.h
+++ HR_States.h
@@ -1,33 +1,34 @@
-enum HRStates {HR_Record, HR_Update, HR_Wait};
+enum HRStates {
+  HR_Record, HR_Update, HR_Wait};
 
 //Current State
 HRStates HRState;
 
 // Sets the initial state
 void InitializeHR() {
-	HrState = HR_Wait;
+  HRState = HR_Wait;
 }
 
 HRStates HRNextState(int reading) {
-	switch (HrState) {
-		case HR_Record:
-			if (beatCounter == 6) {
-				HRState = HR_Update;
-			}
-			break;
-		case HR_Update:
-			HrState = HR_Wait;
-			break;
-		case HR_Wait:
-			lastBeatMillis = 0;
-			totalInterval = 0;
-			beatCounter = 0;
-			if (reading > BEAT_THRESHOLD) {
-				HrState = HR_Record;
-				lastBeatMillis = millis();
-			}
-			break;
-	}
+  switch (HRState) {
+  case HR_Record:
+    if (beatCounter == 6) {
+      HRState = HR_Update;
+    }
+    break;
+  case HR_Update:
+    HRState = HR_Wait;
+    break;
+  case HR_Wait:
+    lastBeatMillis = 0;
+    totalInterval = 0;
+    beatCounter = 0;
+    if (reading > BEAT_THRESHOLD) {
+      HRState = HR_Record;
+      lastBeatMillis = millis();
+    }
+    break;
+  }
 
-	return HRState;
+  return HRState;
 }
--- HeartRate.ino
+++ HeartRate.ino
@@ -1,4 +1,5 @@
 #include <LiquidCrystal.h>
+#include <TimerOne.h>
 
 // LCD screen
 LiquidCrystal LcdDriver( A5, A4, 5, 6, 7, 8 );
@@ -8,7 +9,7 @@ unsigned long Timer;
 const short INTERVAL = 25;
 
 // Beat Detection Threshold
-const int BEAT_THRESHOLD = 650;
+const int BEAT_THRESHOLD = 550;
 // How long to wait to enable beat detection
 const int IGNORE_TIME = 200;
 
@@ -21,44 +22,50 @@ unsigned short beatCounter = 0;
 #include "HR_States.h"
 
 void setup() {
-	// Sets up serial port for debug
-	Serial.begin(9600);
+  // Sets up serial port for debug
+  Serial.begin(9600);
+ 
+  pinMode(10, OUTPUT);
 
-	// Timer for heartrate monitor polling
-	Timer = millis();
+  // Timer for heartrate monitor polling
+  Timer = millis();
 
-	// Sets up LCD for outputting BPM to screen
-	LcdDriver.begin(16,2);
-	
-	// Sets up the state machine
-	InitializeHR();
+  // Sets up LCD for outputting BPM to screen
+  LcdDriver.begin(16,2);
+
+  // Sets up the state machine
+  InitializeHR();
+  Timer1.initialize(1000);
+  Timer1.pwm(10, 512);
 }
 
 void loop()
 {
-	if (millis() - Timer >= INTERVAL) {
-		unsigned int reading = analogRead(0);
-		switch(HRNextState(reading)) {
-			case HR_Record:
-				if (millis() - lastBeatMillis > IGNORE_TIME && reading > BEAT_THRESHOLD) {
-					beatCounter++;
-					totalInterval += millis() - lastBeatMillis;
-					lastBeatMillis = millis();
-				}
-				break;
-			case HR_Update:
-				LcdDriver.clear();
-				LcdDriver.print(CalculateBPM() + " BPM");
-				break;
-			case HR_Wait:
-				break;
-		}
+  if (millis() - Timer >= INTERVAL) {
+    unsigned int reading = analogRead(0);
+    Timer1.pwm(10, reading);
+    switch(HRNextState(reading)) {
+    case HR_Record:
+      if (millis() - lastBeatMillis > IGNORE_TIME && reading > BEAT_THRESHOLD) {
+        beatCounter++;
+        totalInterval += millis() - lastBeatMillis;
+        lastBeatMillis = millis();
+      }
+      break;
+    case HR_Update:
+      LcdDriver.clear();
+      LcdDriver.print((int)CalculateBPM());
+      LcdDriver.print(" BPM");
+      break;
+    case HR_Wait:
+      break;
+    }
 
 
-	Timer += INTERVAL;
-	}
+    Timer += INTERVAL;
+  }
 }
 
 double CalculateBPM() {
-	return 60000 / ((double)totalInterval / 6);
+  return 60000 / ((double)totalInterval / 6);
 }
